f6c3f997a4bce363be7db34e1609fa8732539bcd
Diego <diego@example.com>
2026-04-28 00:14:40 -0300

    refactor(vitest): simplify configuration and remove mergeConfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,26 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
+import { fileURLToPath, URL } from 'node:url';
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vitest/config';
 
-export default mergeConfig(
-    viteConfig,
-    defineConfig({
-        test: {
-            environment: 'jsdom',
-            globals: false,
-            include: ['resources/js/**/*.{spec,test}.ts'],
+export default defineConfig({
+    plugins: [
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }) as any,
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
         },
-    }),
-);
+    },
+    test: {
+        environment: 'jsdom',
+        globals: false,
+        include: ['resources/js/**/*.{spec,test}.ts'],
+    },
+});
